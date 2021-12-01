ae162c178fc9ab2ac53b34734d46c92ec3a16f00
Manager singh <managersingh@outlook.com>
2021-12-01 11:54:45 +0530

    =ccommit in my branch

--- src/api/controllers/admin/industry.controller.js
+++ src/api/controllers/admin/industry.controller.js
@@ -3,7 +3,7 @@ const { omit } = require('lodash');
 const Industry = require('../../models/admin/industry.model');
 
 /**
- * Create new Industry new 
+ * Create new Industry new mybranch
  * @public
  */
  exports.create = async (req, res, next) => {
