f4598bf411da8cf94e144fd3b2408c433d42ea06
Manager singh <managersingh@outlook.com>
2022-03-02 11:07:10 +0530

    API Routes updates

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -4,13 +4,21 @@ const authRoutes = require('./auth.route');
 const industryRoutes = require('./admin/industry.route');
 const jobTitleRoutes = require('./admin/jobTitle.route');
 const countryRoutes = require('./common/country.route');
+const hobbyRoutes = require('./admin/hobby.route');
+const profileRoutes = require('./user/profileSteps.route');
+const serviceNeedRoutes = require('./admin/serviceNeed.route');
+const serviceOfferingRoutes = require('./admin/serviceOffering.route');
+const companyRoutes = require('./admin/company.route');
+const collegeRoutes = require('./admin/college.route');
+const groupRoutes = require('./groupCommunity/groupCommunity.route');
+const groupDicussionRoutes = require('./groupCommunity/groupDiscussion.route');
 
 const router = express.Router();
-
+ 
 /**
  * GET v1/status
  */
-router.get('/status', (req, res) => res.send('OK'));
+router.get('/status', (req, res) => res.send('OK')); 
 
 /**
  * GET v1/docs
@@ -22,6 +30,14 @@ router.use('/auth', authRoutes);
 router.use('/industry', industryRoutes);
 router.use('/jobtitle', jobTitleRoutes);
 router.use('/country', countryRoutes);
+router.use('/hobby', hobbyRoutes);
+router.use('/profile', profileRoutes); 
+router.use('/serviceNeed', serviceNeedRoutes);
+router.use('/serviceOffering', serviceOfferingRoutes);
+router.use('/company', companyRoutes);
+router.use('/college', collegeRoutes);
+router.use('/group',groupRoutes);
+router.use('/',groupDicussionRoutes);
 
 
 module.exports = router;
